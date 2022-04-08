baf55a0324ff05100741f25ce5bebe71ceaa3085
Mehmet Akın Elden <akin.elden@icrontech.com>
2022-04-08 23:42:28 +0300

    Minor improvements and comments

--- mlp_with_tanh.py
+++ mlp_with_tanh.py
@@ -1,4 +1,5 @@
 from collections import deque
+import pandas as pd
 import numpy as np
 import random
 import gym
@@ -41,6 +42,7 @@ class Network:
         self.outputs = []
 
     def forward(self, x):
+        # keep both activations and outputs in the memory
         self.activations = [x.copy()]
         self.outputs = [x.copy()]
         for i in range(len(self.W)-1):
@@ -55,30 +57,25 @@ class Network:
         return x
 
     def backward(self, predictions, targets):
+        # n_x is the batch size
         n_x = predictions.shape[0]
-        d_layer_errors = [0 for i in range(len(self.W))]
-        d_layer_errors[-1] = 2 * (predictions - targets)
-        self.gradW[-1] = (self.outputs[-2].T @ d_layer_errors[-1]) / n_x
-        self.gradb[-1] = ( np.ones((1,n_x)) @ d_layer_errors[-1] ) / n_x
-        for i in range(-2, -len(self.W), -1): # reverse iterate hidden layers
-            d_layer_errors[i] = (1-self.outputs[i]**2)*(d_layer_errors[i+1] @ self.W[i+1].T)
-            self.gradW[i] = (self.outputs[i-1].T @ d_layer_errors[i]) / n_x
-            self.gradb[i] = (np.ones((1,n_x)) @ d_layer_errors[i]) / n_x
-        i-=1
-        d_layer_errors[i] = (d_layer_errors[i+1] @ self.W[i+1].T)
-        self.gradW[i] = (self.outputs[i-1].T @ d_layer_errors[i]) / n_x 
-        self.gradb[i] = (np.ones((1,n_x)) @ d_layer_errors[i]) / n_x 
+        # keep layer errors
+        layer_errors = [0 for i in range(len(self.W))]
+        # output layer error is 2 * (y_pred - y_targ)
+        layer_errors[-1] = 2 * (predictions - targets)
+        self.gradW[-1] = (self.outputs[-2].T @ layer_errors[-1]) / n_x
+        self.gradb[-1] = ( np.ones((1,n_x)) @ layer_errors[-1] ) / n_x
+        for i in range(-2, -len(self.W)-1, -1): # reverse iterate hidden layers
+            # since derivative of tanh(x) is (1-tanh(x)**2) directly use output instead of tanh(activation)  
+            layer_errors[i] = (1-self.outputs[i]**2)*(layer_errors[i+1] @ self.W[i+1].T)
+            self.gradW[i] = (self.outputs[i-1].T @ layer_errors[i]) / n_x
+            self.gradb[i] = (np.ones((1,n_x)) @ layer_errors[i]) / n_x
         return
 
     def update(self, lr):
         for i in range(len(self.W)):
             self.W[i] = self.W[i] - lr * self.gradW[i]
             self.b[i] = self.b[i] - lr * self.gradb[i]
-    
-    def reset_gradients(self):
-        for i in range(len(self.W)):
-            self.gradW[i] = np.zeros_like(self.W[i])
-            self.gradb[i] = np.zeros_like(self.b[i])
 
     def copy_weights_from(self, from_net):
         self.W = from_net.W.copy()
@@ -107,54 +104,72 @@ class DQN:
         self.scores = []
         self.replay_memory = deque(maxlen=replay_memory_size)
 
-    def train(self, n_episode, batch_size=4, target_update_steps=64, min_replay_memory=500, min_epsilon=0.1, log = True):
+    def train(self, n_episode, batch_size=4, target_update_steps=64, min_epsilon=0.1, log = True):
+        batch_size = min(batch_size, self.replay_memory.maxlen)
         episode = 0
         iter = 0
         while episode < n_episode:
+            # initialize for each episode:
             episode += 1
             score = 0
             done = False
             observation = self.env.reset()
             while not done:
+                # for each step in episode
                 iter += 1
+                # first forward pass using current observation
                 output = self.training_network.forward(observation)
+                # then select action with epsilon greedy
                 action = np.argmax(output) if random.random() > self.epsilon else self.env.action_space.sample()
+                # perform action and observe new state
                 new_observation, reward, done, _info = self.env.step(action)
                 score += reward
+                # add state information to replay memory
                 self.replay_memory.append((observation, action, reward, done, new_observation))
+                # set current observation as the new observation
                 observation = new_observation
                 
                 if done:
+                    # if episode is ended, add score and update epsilon
                     self.scores.append(score)
                     self.epsilon = max(min_epsilon, self.epsilon*self.decay)
                     if log: print(f"Episode {episode} : {score}")
-
-                if len(self.replay_memory) < min(min_replay_memory, self.replay_memory.maxlen): # Then we don't have enough memory, continue playing without learning
+                # If we don't have enough memory, continue playing without learning
+                if len(self.replay_memory) < self.replay_memory.maxlen: 
                     continue
                 
-                # learning with mini batches
+                # select random sample from replay memory
                 old_observations, actions, rewards, dones, new_observations = zip(*random.sample(self.replay_memory, batch_size))
                 
+                # get the q value of new state by using target network
                 q_news = np.max(self.target_network.forward(np.array(new_observations)), axis=1)
+                # get the target value as reward or reward + q_new according to done condition
                 target_values = np.array(rewards) + self.gamma * (1-np.array(dones)) * q_news
-                # target_values = [reward_ if done_ else reward_ + self.gamma * q_new_ for reward_,done_,q_new_ in zip(rewards, dones, q_news) ]
 
+                # get the predictions using training network
                 predictions = self.training_network.forward(np.array(old_observations))
+                
+                # copy predictions as targets, and update the values corresponding to actions with the 
+                # above calculated target values
                 targets = predictions.copy()
                 targets[np.arange(len(actions)), np.array(actions)] = target_values
-                # for i, action in enumerate(actions) : targets[i, action] = target_values[i]
+                
+                # apply backpropagation and update the network
                 self.training_network.backward(predictions, targets)
                 self.training_network.update(self.learning_rate)
 
+                # update target network if enough steps are taken
                 if iter % target_update_steps == 0:
                     self.target_network.copy_weights_from(self.training_network)
 
-        avg_scores = np.mean(np.array(self.scores[len(self.scores) % 10:]).reshape(-1, 10), axis=1)
-        fig, axs = plt.subplots(1, 2, figsize=(12,6))
-        axs[0].plot(self.scores)
-        axs[0].set_title("Individual Scores")
-        axs[1].plot(avg_scores)
-        axs[1].set_title("10-Averaged Scores")
+    def plot_scores(self, roll_size=100):
+        data = pd.DataFrame({"scores" : self.scores})
+        data["avg"] = data.scores.rolling(roll_size).mean()
+        plt.figure(figsize=(12,6))
+        plt.xlabel = "Episode"
+        plt.ylabel = "Score"
+        plt.plot(data["scores"], color="red", alpha=0.2)
+        plt.plot(data["avg"], color="blue")
         plt.show()
     
     def run_visual(self, n_episode):
